36668eaac6a0e86270cb5d9d0dff596d8bfb6ff4
RJJohnson162 <91172411+RJJohnson162@users.noreply.github.com>
2023-06-26 22:03:42 +0300

    Final-touches

--- frontend_sanity/src/components/SocialMedia.jsx
+++ frontend_sanity/src/components/SocialMedia.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import { BsTwitter, BsInstagram} from 'react-icons/bs';
+import React from 'react';
+import { BsTwitter, BsInstagram } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => {
+  const handleLinkClick = (url) => {
+    window.open(url, '_blank'); // Opens the link in a new tab
+  };
+
   return (
     <div className='app__social'>
-        <div>
-            <BsTwitter/>
-        </div>
-        <div>
-            <BsInstagram/>
-        </div>
-        <div>
-            <FaFacebookF/>
-        </div>
+      <div onClick={() => handleLinkClick('https://twitter.com/MarubiRichard')}>
+        <BsTwitter />
+      </div>
+      <div onClick={() => handleLinkClick('https://www.instagram.com/rj_johnson_gse/')}>
+        <BsInstagram />
+      </div>
+      <div onClick={() => handleLinkClick('https://www.facebook.com/profile.php?id=100067903295999')}>
+        <FaFacebookF />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
